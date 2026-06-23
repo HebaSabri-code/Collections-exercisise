a5a583361a453e3c5a48565963ae32a6be67bca7
Your Name <hebsab000@gmail.com>
2026-06-23 12:45:35 +0300

    Update Collections exercises

--- src/Main/ShoppingList.java
+++ src/Main/ShoppingList.java
@@ -1,4 +1,35 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class ShoppingList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> shoppingList = new ArrayList<>();
+
+        // Add items
+        shoppingList.add("Milk");
+        shoppingList.add("Bread");
+        shoppingList.add("Eggs");
+        shoppingList.add("Juice");
+        shoppingList.add("Rice");
+
+        // Print list
+        System.out.println("Shopping List:");
+
+        for (int i = 0; i < shoppingList.size(); i++) {
+            System.out.println((i + 1) + ". " + shoppingList.get(i));
+        }
+
+        // Remove item
+        shoppingList.remove("Juice");
+
+        // Print updated list
+        System.out.println("\nUpdated List:");
+
+        for (int i = 0; i < shoppingList.size(); i++) {
+            System.out.println((i + 1) + ". " + shoppingList.get(i));
+        }
+    }
 }
